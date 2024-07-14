c2a41452a92bcd879d9b86c8580f922054284b4d
claw5ie <92851066+claw5ie@users.noreply.github.com>
2024-07-14 19:10:01 +0100

    Check if colors are supported

--- test.sh
+++ test.sh
@@ -2,12 +2,27 @@
 
 set -eu
 
-parser_tests="tests/parser/*.test"
-typechecker_tests="tests/typechecker/*.test"
-ircode_tests="tests/*.test"
+red="\e[31m"
+green="\e[32m"
+reset="\e[0m"
+
+if [ -t 1 ]; then
+    colors=$(tput colors)
+    if [ $colors -eq -1 ]; then
+        red=""
+        green=""
+        reset=""
+    fi
+fi
+
+{
+    parser_tests="tests/parser/*.test"
+    typechecker_tests="tests/typechecker/*.test"
+    ircode_tests="tests/*.test"
+}
 tests="${parser_tests} ${typechecker_tests} ${ircode_tests}"
 total_count=0
-failed_count=0
+passed_count=0
 
 for file in $tests
 do
@@ -23,20 +38,21 @@ do
         expected=$(cat "${base_file_path}.expect")
 
         if [ "$output" == "$expected" ]; then
-            echo -e "\e[32mOK\e[0m"
+            echo -e "${green}OK${reset}"
+            ((passed_count += 1))
         else
-            echo -e "\e[31mFAILED\e[0m"
-            ((failed_count += 1))
+            echo -e "${red}FAILED${reset}"
         fi
     else
         echo -e "\e[31mmissing expected output\e[0m"
-        ((failed_count += 1))
     fi
 done
 
-echo ""
-echo "Failed ${failed_count}/${total_count} tests"
+failed_count=$((total_count-passed_count))
 
-if [ $failed_count -gt 0 ]; then
+echo ""
+echo -e "${green}Passed${reset}/${red}Failed${reset}/Total ----- ${green}${passed_count}${reset}/${red}${failed_count}${reset}/${total_count} tests"
+
+if [ $passed_count -ne $total_count ]; then
     exit 1
 fi
